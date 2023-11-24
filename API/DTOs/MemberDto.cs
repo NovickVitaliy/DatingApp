6cf54924980b41627bf27304bad13366dcf1328f
Vitalii Novik <novickvitaliy@gmail.com>
2023-11-25 01:40:14 +0200

    fix

--- API/DTOs/MemberDto.cs
+++ API/DTOs/MemberDto.cs
@@ -10,7 +10,7 @@ public class MemberDto
     public int Age { get; set; }
     public string KnownAs { get; set; }
     public DateTime Created { get; set; }
-    public DateTime LactActive { get; set; }
+    public DateTime LastActive { get; set; }
     public string Gender { get; set; }
     public string Introduction { get; set; }
     public string LookingFor { get; set; }
